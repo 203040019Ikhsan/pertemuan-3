21d4bce5d00376312bc7de203b110d2936f4e991
Ikhsan Ardiansyah <72891992+203040019Ikhsan@users.noreply.github.com>
2021-03-02 01:06:39 +0700

    Add files via upload

---
+++ latihan3.php
@@ -0,0 +1,44 @@
+<?php
+// pengulangan
+// for
+// while
+// do.. while
+// foreach : pengulangan khusus array
+
+// for( $i = 0; $i < 5; $i++ ) {
+    // echo "Hello world! <br>";
+// }
+// $i = 0;
+// while( $i < 5 ) {
+//     echo "Hello world! <br>";
+// $i++;
+// }
+// $i = 10;
+// do {
+//     echo "Hello World! <br>";
+// $i++;
+// } while( $i < 5 );
+
+// pengkodingan / percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+// $x = 30;
+// if( $x < 20 ) {
+//     echo "benar";
+// } else if( $x == 20 ) {
+//     echo "bingo!";
+// } else {
+//     echo "salah";
+// }
+
+
+
+
+
+
+
+
+?>
